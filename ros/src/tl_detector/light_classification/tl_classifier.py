d91111cc1305ccdcf56d1d49ed283208add1353f
Pavel Silin <psilin@topcon.com>
2018-11-20 20:49:31 +0300

    tl_detector opencv based red & yellow traffic light detection

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,6 @@
 from styx_msgs.msg import TrafficLight
+import numpy as np
+import cv2
 
 class TLClassifier(object):
     def __init__(self):
@@ -16,4 +18,36 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        lower_red = np.array([0, 100, 100])
+        upper_red = np.array([10, 255, 255])
+        red1 = cv2.inRange(hsv, lower_red, upper_red)
+
+        lower_red = np.array([170, 100, 100])
+        upper_red = np.array([180, 255, 255])
+        red2 = cv2.inRange(hsv, lower_red, upper_red)
+
+        converted_img_red = cv2.addWeighted(red1, 1.0, red2, 1.0, 0.0)
+
+        blur_img_red = cv2.GaussianBlur(converted_img_red, (15,15), 0)
+
+        circles_red = cv2.HoughCircles(blur_img_red, cv2.HOUGH_GRADIENT, 0.5, 41, param1=70, param2=30, minRadius=5, maxRadius=150)
+
+        if circles_red is not None:
+            return TrafficLight.RED
+
+        lower_yellow = np.array([20, 100, 100])
+        upper_yellow = np.array([40, 255, 255])
+        yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+
+        blur_img_yellow = cv2.GaussianBlur(yellow, (15,15), 0)
+
+        circles_yellow = cv2.HoughCircles(blur_img_yellow, cv2.HOUGH_GRADIENT, 0.5, 41, param1=70, param2=30, minRadius=5, maxRadius=150)
+
+        if circles_yellow is not None:
+            return TrafficLight.YELLOW
+
+        # Do not differentiate green and unknown
         return TrafficLight.UNKNOWN
+
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -140,16 +140,19 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        return light.state
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return False
 
-        #if(not self.has_image):
-        #    self.prev_light_loc = None
-        #    return False
-
-        #cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         # Get classification
-        #return self.light_classifier.get_classification(cv_image)
+        result = self.light_classifier.get_classification(cv_image)
+        #rospy.loginfo("STATE: ")
+        #rospy.loginfo(light.state)
+        #rospy.loginfo(result)
+
+        return result
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
